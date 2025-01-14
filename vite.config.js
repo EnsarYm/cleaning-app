e530a837e4728b8cf52e84ce0f7999a6151e48c8
Ceren Çotuk <cerencotuk@Ceren-MacBook-Air.local>
2025-01-14 19:54:23 +0300

    feat: update laravel project

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000
-  },
-  base: '/'
-}) 
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+});
